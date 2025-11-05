82c9dfa92a1a46147b8d9f9a81aede9bafd70548
Abhishekvp932 <avp7580@gmail.com>
2025-11-05 20:57:45 +0530

    fix : home page issues

--- front-end/src/pages/User/Home.tsx
+++ front-end/src/pages/User/Home.tsx
@@ -71,7 +71,7 @@ interface userInfo{
               </div>
               
               <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold leading-tight">
-                Your Health,
+                Your Health,Abhishek vp suttu mone ann gooyes
                 <span className="block text-blue-200">Our Priority</span>
               </h1>
               
