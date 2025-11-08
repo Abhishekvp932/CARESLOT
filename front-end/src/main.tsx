d92596a34250870e9be5dc0b961da49dd546ed2b
Abhishekvp932 <avp7580@gmail.com>
2025-11-08 17:51:26 +0530

    converted to pwa

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -17,3 +17,13 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>,
 )
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(reg => console.log("Service Worker registered:", reg))
+      .catch(err => console.log("Service Worker failed:", err));
+  });
+}
+
