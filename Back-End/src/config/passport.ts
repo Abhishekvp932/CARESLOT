69786967f4e5cabeff7858917f8abca3a22c7c3c
Abhishekvp932 <avp7580@gmail.com>
2025-11-08 13:20:09 +0530

    feat : google auth

--- Back-End/src/config/passport.ts
+++ Back-End/src/config/passport.ts
@@ -10,17 +10,13 @@ import { IBaseUser } from '../utils/IBaseUser';
 dotenv.config();
 
 export const confiqurePassport = () => {
-  const GOOGLE_CALLBACK_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.GOOGLE_CALLBACK_URL_PROD
-    : process.env.GOOGLE_CALLBACK_URL_DEV;
 
   passport.use(
     new GoogleStrategy(
       {
         clientID: process.env.GOOGLE_CLIENT_ID!,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        callbackURL:GOOGLE_CALLBACK_URL,
+        callbackURL:process.env.GOOGLE_CALLBACK_URL,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
