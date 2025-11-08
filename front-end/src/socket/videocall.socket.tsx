720cc84e597d4c4e3d0594ca2ffbf7f641c8bcde
Abhishekvp932 <avp7580@gmail.com>
2025-11-08 15:18:41 +0530

    feat : google auth

--- front-end/src/socket/videocall.socket.tsx
+++ front-end/src/socket/videocall.socket.tsx
@@ -1,7 +1,7 @@
 
 import { io, Socket } from "socket.io-client";
 
-export const socket: Socket = io("http://localhost:3000/video-call", {
+export const socket: Socket = io("/video-call", {
   withCredentials: true,
   transports: ["websocket"],
 });
