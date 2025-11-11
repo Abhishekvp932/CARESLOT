f5f5c26a23db73b12563f70ef764925b103811b9
Abhishekvp932 <avp7580@gmail.com>
2025-11-11 13:25:45 +0530

    fix : home page

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 export default defineConfig({
+  base:'./',
  server:{
   port:2025,
    strictPort: true,
