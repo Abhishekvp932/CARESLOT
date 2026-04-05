626e55f46e2444d4958d106eb0dd9adcd8d52a6a
Abhishekvp932 <avp7580@gmail.com>
2026-04-05 16:12:26 +0530

    fix some server.js change cors changed

--- Back-End/src/server.ts
+++ Back-End/src/server.ts
@@ -85,6 +85,8 @@ const corsOperation = {
 //   origin: 'http://localhost:2025',
 //   credentials: true,
 // };
+app.use(cors(corsOperation));
+app.options('*', cors(corsOperation));
 app.use(cookieParser());
 app.use(
   session({
@@ -97,7 +99,6 @@ app.use(
     },
   })
 );
-app.use(cors(corsOperation));
 app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
